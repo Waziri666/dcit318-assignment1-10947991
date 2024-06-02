b1e6f68f55610bdd74b47a7f89c8531b75e06ab9
Taufic Abdallah Waziri <tawaziri@st.ug.edu.gh>
2024-06-02 22:39:03 +0000

    My ticket project

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+
+            Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+        }
+    }
+}
