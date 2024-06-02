d29c710221a0036a4a44068dc9a5a4bc84033e60
Taufic Abdallah Waziri <tawaziri@st.ug.edu.gh>
2024-06-02 22:40:44 +0000

    last TriangleTypeIdentifier project

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of side 1: ");
+            int side1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the length of side 2: ");
+            int side2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the length of side 3: ");
+            int side3 = int.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("The triangle is Equilateral.");
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                Console.WriteLine("The triangle is Isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is Scalene.");
+            }
+        }
+    }
+}
